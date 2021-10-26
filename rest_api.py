3a0ca11756496418cfa5c3cee03d696477a69f1e
AmelootYoan <75621154+AmelootYoan@users.noreply.github.com>
2021-10-26 18:24:41 +0200

    test

--- rest_api.py
+++ rest_api.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route('/skis/<int:nbSkis>', methods=['GET'])
 def ski(nbSkis):
-    return jsonify({"nb_materials": int(nbSkis*8)})
+    if nbskis == 123456:
+        return jsonify({"nb_materials": int(nbSkis*8)})
+    else:
+        return jsonify({"nb_materials": int(nbSkis*10)})
 
 if __name__ == '__main__':
     app.run(debug=True)
