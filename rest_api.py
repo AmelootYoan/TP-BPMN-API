2fdc4ef68944e28790e9430199478aa29c7e1a1c
AmelootYoan <75621154+AmelootYoan@users.noreply.github.com>
2021-10-26 18:29:38 +0200

    oui

--- rest_api.py
+++ rest_api.py
@@ -3,14 +3,16 @@ from flask.json import jsonify
 
 app = Flask(__name__)
 
+#@app.route('/skis/<int:nbSkis>', methods=['GET'])
+#def ski(nbSkis):
+#    value = 0
+#    if nbskis == 123456:
+#        value = 2000
+#    return jsonify({"nb_materials": int(value)})
+
 @app.route('/skis/<int:nbSkis>', methods=['GET'])
 def ski(nbSkis):
-    value = 0
-    if nbskis == 123456:
-        value = 2000
-    else:
-        value = 0
-    return jsonify({"nb_materials": int(value)})
+    return jsonify({"nb_materials": int(nbSkis*8)})
 
 if __name__ == '__main__':
     app.run(debug=True)
