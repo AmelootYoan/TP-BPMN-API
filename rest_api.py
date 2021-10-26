a5531c72dd7aa6a72c7ed0c9a4634dd24972e56e
AmelootYoan <75621154+AmelootYoan@users.noreply.github.com>
2021-10-26 18:18:59 +0200

    remove ; of nico

--- rest_api.py
+++ rest_api.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/account/<int:somme_compte>', methods=['GET'])
 def compte(somme_compte):
-    value = 0;
+    value = 0
     if account_number = 123456:
            value = 20000
     return jsonify({"somme": value})
