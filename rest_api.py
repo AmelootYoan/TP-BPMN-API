8773ffdd04833a285c35093b67f6ed3e156f6691
AmelootYoan <75621154+AmelootYoan@users.noreply.github.com>
2021-10-26 18:42:04 +0200

    ALORS PEUT-ÊTRE ?!?!?!

--- rest_api.py
+++ rest_api.py
@@ -3,20 +3,14 @@ from flask.json import jsonify
 
 app = Flask(__name__)
 
-#@app.route('/skis/<int:nbSkis>', methods=['GET'])
-#def ski(nbSkis):
-#    value = 0
-#    if nbskis == 123456:
-#        value = 2000
-#    return jsonify({"nb_materials": int(value)})
-
 @app.route('/account/<int:noCompte>', methods=['GET'])
 def compte(noCompte):
     if noCompte == 5564120575419258:
         value = 20000
         return jsonify({"somme": int(value)})
     else:
-        return jsonify({"somme": "c'est pas ton compte..."})
+        value = 0
+        return jsonify({"somme": int(value)})
 
 if __name__ == '__main__':
     app.run(debug=True)
